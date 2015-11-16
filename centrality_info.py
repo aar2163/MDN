7a3cfbcceee5ea599f1cea999e40c900d90ebb72
root <root@mdn.cheme.columbia.edu>
2015-11-16 11:51:46 -0500

    comments

--- centrality_info.py
+++ centrality_info.py
@@ -5,6 +5,11 @@ import math
 import mdn
 from sets import Set
 
+"""
+ This script returns available groups for network analysis to analysis.php
+ Usage: centrality_info.py ticket
+"""
+
 def get_effic(a,b):
  a = a.flatten()
  b = b.flatten()
@@ -24,8 +29,11 @@ netset = Set(mdn.dic2list(data['network']['atoms']['nr']))
 for ii,n in enumerate(names):
  s = Set(mdn.dic2list(groups[str(ii)]['atoms']))
 
+ # We skip this group if it is a network node
  if (re.match(mdn.renode,n)):
   continue
+
+ # print info with required format
  print ii,n,((s.issubset(netset) and  groups[str(ii)]['network_ok']) or groups[str(ii)]['network_specified'])
  #if(ii == 18):
  # print s
